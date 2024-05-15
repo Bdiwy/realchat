8d91bf936501660a6dfb483d54ca0572ba469fa2
Ahmed bdiwy <ahmedbedawi22@gmail.com>
2024-05-15 14:04:40 +0300

    first commit

--- index.js
+++ index.js
@@ -11,11 +11,20 @@ application.use(express.static('public_html'));
 var sio = socket(server);
 
 sio.on('connection',function(visitor){
-
 	console.log('we have a new visitor as id=>',visitor.id);
 
+	// visitor.on('message', function (data) {
+    //     // Here you can filter the chat members and send the message accordingly
+    //     data.RealTimeResponse.chatmembers.forEach(member => {
+    //         if (data.RealTimeResponse.chat_id == data.chat_id) {
+    //             // sio.to(member.socketId).emit('new_msg', data);
+	// 			// sio.sockets.emit('new_msg',data);
+    //         }
+    //     });
+    // });
+
 	visitor.on('message',function(data){
-		sio.sockets.emit('new_msg',data); 
+			sio.sockets.emit('new_msg',data); 
 	});
 
 	
--- public_html/client.js
+++ public_html/client.js
@@ -1,29 +1,73 @@
 var socket = io.connect('http://localhost:5000');
 
-var username  = document.getElementById('username');
-var message   = document.getElementById('message');
-var send 	  = document.getElementById('send');
-var chat 	  = document.getElementById('chat');
+var chat_id  = document.querySelector('#chat_id');
+var body   = document.querySelector('.message-input');
+var send 	  = document.getElementById('sendMessageBtn');
+var chat 	  = document.querySelector('.chat-messages-list');
 var boradcast 	  = document.getElementById('boradcast');
 
-send.addEventListener('click',function(){
-	socket.emit('message',{
-		username:username.value,
-		message:message.value,
-	});
+send.addEventListener('click', function () {
+
+    socket.emit('message', {
+        RealTimeResponse:RealTimeResponse,
+        messagememberid:RealTimeResponse.memberid,
+        currentUserId: currentUserId,
+        chat_id: chat_id.value,
+        body: body.value,
+        imagePath :imagePath  ,
+        imageFilename :imageFilename  ,
+        message :message ,
+    });
+
 });
 
 
-message.addEventListener('keypress',function(){
-	socket.emit('borad',{
-		username:username.value
-	});
-});
+// message.addEventListener('keypress',function(){
+// 	socket.emit('borad',{
+// 		chat_id:chat_id.value
+// 	});
+// });
  
 socket.on('new_msg',function(data){
-  boradcast.innerHTML = '';
- chat.innerHTML += '<div class="container"><strong>'+data.username+':</strong>'+data.message+'</div>';
+//  boradcast.innerHTML = '';
+    handleNewMessage(data) ;
 });
+
+
+function handleNewMessage(data) {
+    console.log(data);
+    console.log(data.chat_id);
+    console.log(data.RealTimeResponse.chatId);
+    if (data.RealTimeResponse.chatId == data.chat_id && data.RealTimeResponse.chatmembers.memberid == data.messagememberid) {
+    var isthismyMessage = data.messagememberid == RealTimeResponse.memberid ;
+    var dropdown= `<div class="dropdown">
+                                <button class="btn p-0" type="button" id="chat-header-actions"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="bx bx-dots-vertical-rounded fs-4"></i>
+                                    </button>
+                                <div class="dropdown-menu dropdown-menu-start" aria-labelledby="chat-header-actions">
+                                    <a href="javascript:void(0)"  style="color:red; text-align:center;" data-bs-toggle="modal" data-bs-target="#deletemessage" onclick="deleteMessage(${data.message})"> <i class="bx bx-trash-alt"></i>Delete </a>
+                            </div>
+                            </div>`;
+                    chat.innerHTML += `
+                    <li class="chat-message ${isthismyMessage ? 'chat-message-right' : 'chat-message-left' } ">
+                                    <div class="d-flex overflow-hidden">
+                                    
+                                ${isthismyMessage ? dropdown :''}
+                                    <div class="chat-message-wrapper flex-grow-1">
+                                        <div class="chat-message-text" style="${isthismyMessage ? 'margin-right:20px' : 'margin-left:20px'} ;" >
+                                        <div class="chat-sender-name text-muted mb-1" style="cursor: pointer;">${!isthismyMessage ? 'data.message.name' : ''}</div>
+                                            <div class="file-details" style="padding:10px; padding-left:3px; ">
+                                            <b><p class="file-info-line">
+                                                <span class="file-name" ${isthismyMessage ? 'style="color:white"' : 'style="color: black ; postion:center;"'}>${data.body}</span>
+                                            </p></b>
+                                        </div>
+                                        </div>									
+                                </div>
+                                </div>
+                                </div>`;
+                            }};
+
 
 
 socket.on('new_borad',function(data){
