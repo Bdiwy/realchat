d6d3f845ae67b6404a41da72e769e2fc41d8ea71
Ahmed bdiwy <ahmedbedawi22@gmail.com>
2024-05-27 12:10:50 +0300

    formating lines

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var socket  = require('socket.io');
+var socket = require('socket.io');
 
 var application = express();
-var server = application.listen(5000,function(){
+var server = application.listen(5000, function () {
 	console.log('Your Server Is runing at http:/localhost:5000');
 });
 
@@ -10,21 +10,21 @@ application.use(express.static('public_html'));
 
 var sio = socket(server);
 
-sio.on('connection',function(visitor){
-	console.log('we have a new visitor as id=>',visitor.id);
+sio.on('connection', function (visitor) {
+	console.log('we have a new visitor as id=>', visitor.id);
 
-	visitor.on('message',function(data){
-			sio.sockets.emit('new_msg',data); 
+	visitor.on('message', function (data) {
+		sio.sockets.emit('new_msg', data);
 	});
-	visitor.on('fileMessage',function(data){
-		sio.sockets.emit('fileMessage',data); 
+	visitor.on('fileMessage', function (data) {
+		sio.sockets.emit('fileMessage', data);
 	});
-	visitor.on('deletemessageid',function(data){
-		sio.sockets.emit('new_deletemessageid',data); 
+	visitor.on('deletemessageid', function (data) {
+		sio.sockets.emit('new_deletemessageid', data);
 	});
 
-	visitor.on('borad',function(data){
-		visitor.broadcast.emit('new_borad',data); 
+	visitor.on('borad', function (data) {
+		visitor.broadcast.emit('new_borad', data);
 	});
 
 
